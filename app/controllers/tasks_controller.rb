0f6b544dd3def2235667640786e29c5865bb353d
Sergei_Savelev <lavrentiy83@gmail.com>
2021-05-12 04:40:35 +0300

    upd validate model task & upd task comtroller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.fetch(:task, {})
+      params.require(:task).permit(:title, :description, :datetime)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   validates :title, length: {maximum: 255}
+  validates :description
+  validates :datetime
 end
