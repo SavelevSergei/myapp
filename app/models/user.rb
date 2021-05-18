ae955251ca93455519ac349b7ee3fabe6f4e7227
Sergei_Savelev <lavrentiy83@gmail.com>
2021-05-18 03:25:33 +0300

    add devise for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :tasks
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "User №#{rand(999)}" if self.name.blank?
+  end
 end
