37bf507658a94b0d99a09c2d88b7272ba3116474
Sergei_Savelev <lavrentiy83@gmail.com>
2021-05-23 03:08:23 +0300

    add migrate completed to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_234219) do
+ActiveRecord::Schema.define(version: 2021_05_23_000601) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_234219) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.boolean "completed"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
