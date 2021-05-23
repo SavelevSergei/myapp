7b49d2063cb04148f47e5e51015e3506ca99b0d7
Sergei_Savelev <lavrentiy83@gmail.com>
2021-05-23 03:10:28 +0300

    add migrate priority to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_000601) do
+ActiveRecord::Schema.define(version: 2021_05_23_000928) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_000601) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.boolean "completed"
+    t.integer "priority"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
